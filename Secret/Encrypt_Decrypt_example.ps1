584c46a35fd6c4c18830f8099508a86420ed47ac
Moshikot3 <moshikot015@gmail.com>
2022-02-08 18:09:56 +0200

    Update Encrypt_Decrypt_example.ps1

--- Secret/Encrypt_Decrypt_example.ps1
+++ Secret/Encrypt_Decrypt_example.ps1
@@ -1,9 +1,14 @@
-﻿
-$key = 202,144,73,88,228,1,7,104,95,212,137,87,125,201,80,75,53,18,39,108,60,218,212,151,239,241,34,117,106,184,212,179 | out-file "$($Scriptpath)\Secret\key.key"
-$secret = "aidcopass"
+$ScriptPath = "D:\PostgreSQL"
+
 
 
 # encrypt
+$key = 202,144,73,88,228,1,7,104,95,212,137,87,125,201,80,75,53,18,39,108,60,218,212,151,239,241,34,117,106,184,212,179 | out-file "$($Scriptpath)\Secret\key.key"
+$key = gc "$($Scriptpath)\Secret\key.key"
+$Key -join ","
+
+$secret = "aidcopass"
+
 $secretSecured = ConvertTo-SecureString $secret -AsPlainText -Force
 $encrypted = ConvertFrom-SecureString -secureString $secretSecured -key $key | Out-file "$($Scriptpath)\Secret\Encrypted.txt"
 
